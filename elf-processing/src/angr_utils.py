cf8dc6d10d3a56601c631b353f32cbba1988aaa9
Ryan Bouchou <ryanbouchou@orange.fr>
2026-02-25 21:16:41 +0100

    Update(angr_utils): Documentation

--- elf-processing/src/angr_utils.py
+++ elf-processing/src/angr_utils.py
@@ -2,11 +2,14 @@ import os
 
 import angr
 
-"""
-Ce module contient des fonctions utilitaires pour analyser les CFGs générés par Angr.
-Il permet d'afficher des informations sur les fonctions identifiées, les blocs de base, exceptions, etc. L'objectif est de faciliter la compréhension du CFG et d'aider à l'identification de patterns intéressants.:
-"""
+"""Ce module fournit des fonctions utilitaires pour analyser un binaire avec angr, 
+notamment pour construire et afficher le CFG (Control Flow Graph) du projet."""
+
+
 def print_cfg(cfg: angr.analyses.CFGFast, project: angr.Project) -> None:
+    """
+    Affiche des informations sur le CFG et les fonctions identifiées dans le projet.
+    """
     print(f"\n[+] Analyse de : {cfg.project.filename}")
     print(f"[*] Nombre de nœuds (blocs) dans le CFG : {cfg.graph.number_of_nodes()}")
     print(f"[*] Nombre d'arêtes (sauts) dans le CFG : {cfg.graph.number_of_edges()}")
@@ -17,11 +20,11 @@ def print_cfg(cfg: angr.analyses.CFGFast, project: angr.Project) -> None:
         if not func.is_simprocedure:
             print(f"  - {func.name} à l'adresse {hex(addr)}")
 
-    main_func = project.kb.functions.get('main')
+    main_func = project.kb.functions.get("main")
     if main_func:
         print(f"\n[+] Focus sur 'main' ({hex(main_func.addr)}) :")
         print(f"  - Nombre de blocs de base : {len(main_func.block_addrs_set)}")
-        
+
         start_block = project.factory.block(main_func.addr)
         print("\n[+] Code assembleur du premier bloc de main :")
-        start_block.pp() # Pretty Print de l'assembleur
+        start_block.pp()  # Pretty Print de l'assembleur
